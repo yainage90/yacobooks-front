bda26a0f97391e00cb1cd1dfb29ce0ba2ae60f9c
yaincoding <yaincoding@gmail.com>
2021-09-23 01:30:16 +0900

    suggestion dropdown does not push under contents anymore

--- src/component/desktop/SuggestBox.js
+++ src/component/desktop/SuggestBox.js
@@ -1,22 +1,32 @@
 import React from "react";
 
-import { List } from "antd";
+import { Layout, List } from "antd";
 
 const SuggestBox = ({ titles }) => {
   return (
-    <List
-      size="small"
-      bordered
-      dataSource={titles}
-      renderItem={(item) => <List.Item>{item}</List.Item>}
-      style={suggestBoxStyle}
-    />
+    <Layout style={containerStyle}>
+      <List
+        size="small"
+        bordered
+        dataSource={titles}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+        style={suggestBoxStyle}
+      />
+    </Layout>
   );
+};
+
+const containerStyle = {
+  display: "flex",
+  width: "500px",
 };
 
 const suggestBoxStyle = {
   width: "500px",
   fontFamily: "notosans_regular",
+  background: "#eaeaea",
+  position: "absolute",
+  zIndex: 10,
 };
 
 export default SuggestBox;
