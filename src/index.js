08c110ffec1d1bbc5f1189db60bf576f20e662d8
yaincoding <yaincoding@gmail.com>
2021-09-25 19:44:22 +0900

    axios default baseURL added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import "antd-mobile/dist/antd-mobile.css";
 
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
