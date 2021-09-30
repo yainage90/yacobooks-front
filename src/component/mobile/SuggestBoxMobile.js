3caf4300cf059ca97bb6dd082307372fdefcc271
yaincoding <yaincoding@gmail.com>
2021-10-01 02:17:37 +0900

    cleaned up styles

--- src/component/mobile/SuggestBoxMobile.js
+++ src/component/mobile/SuggestBoxMobile.js
@@ -4,29 +4,27 @@ import { List } from "antd-mobile";
 
 const SuggestBox = ({ titles }) => {
   return (
-    <div
-      style={{
-        margin: "0.3rem 0 0 0",
-      }}
-    >
-      <List>
+    <div class="suggest-container" style={suggestContainerStyle}>
+      <List className="suggest-list">
         {titles.slice(0, Math.min(5, titles.length)).map((title) => (
           <List.Item>
-            <p
-              style={{
-                fontFamily: "notosans_regular",
-                fontSize: "0.8rem",
-                margin: 0,
-                whiteSpace: "pre-line",
-              }}
-            >
-              {title}
-            </p>
+            <p style={suggestItemStyle}>{title}</p>
           </List.Item>
         ))}
       </List>
     </div>
   );
+};
+
+const suggestContainerStyle = {
+  margin: "0.3rem 0 0 0",
+};
+
+const suggestItemStyle = {
+  fontFamily: "notosans_regular",
+  fontSize: "0.8rem",
+  margin: 0,
+  whiteSpace: "pre-line",
 };
 
 export default SuggestBox;
