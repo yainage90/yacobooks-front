474ffe53e664babd0440dd0cff9be0b553dfee36
yaincoding <yaincoding@gmail.com>
2021-09-30 23:56:24 +0900

    cleaned up styles

--- src/component/mobile/BookItemMobile.js
+++ src/component/mobile/BookItemMobile.js
@@ -6,50 +6,47 @@ import missingImage from "../../resources/missing.png";
 
 const BookItemMobile = ({ book }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        margin: "0.3rem",
-      }}
-    >
+    <div style={bookItemContainerStyle}>
       <div>
         <Image
           src={book.imageUrl}
-          width="74"
+          alt={book.title}
+          width="74px"
           height="90px"
           fallback={missingImage}
         />
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "start",
-          margin: "0 0 0 0.4rem",
-        }}
-      >
-        <p
-          style={{
-            fontFamily: "notosans_bold",
-            fontSize: "0.9rem",
-            margin: "0 0 0.3rem 0",
-          }}
-        >
-          {book.title}
-        </p>
-        <p
-          style={{
-            fontFamily: "notosans_medium",
-            fontSize: "0.8rem",
-            color: "#606060",
-          }}
-        >
-          {book.author}
-        </p>
+      <div style={bookInfoContainerStyle}>
+        <p style={bookTitleStyle}>{book.title}</p>
+        <p style={bookAuthorStyle}>{book.author}</p>
       </div>
     </div>
   );
+};
+
+const bookItemContainerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  margin: "0.3rem",
+};
+
+const bookInfoContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "start",
+  margin: "0 0 0 0.4rem",
+};
+
+const bookTitleStyle = {
+  fontFamily: "notosans_black",
+  fontSize: "0.9rem",
+  margin: "0 0 0.3rem 0",
+};
+
+const bookAuthorStyle = {
+  fontFamily: "notosans_medium",
+  fontSize: "0.8rem",
+  color: "#606060",
 };
 
 export default BookItemMobile;
