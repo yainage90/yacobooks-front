56afb32a3684d59a4e175c591668ee16b380a00a
yaincoding <yaincoding@gmail.com>
2021-10-04 02:34:58 +0900

    Added styled wrapper for click effect

--- src/component/mobile/BookListMobile.js
+++ src/component/mobile/BookListMobile.js
@@ -2,18 +2,28 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import styled from "styled-components";
+
 import { List } from "antd-mobile";
 
 import BookItemMobile from "./BookItemMobile";
+
+const BookItemContainer = styled.div`
+  &:hover {
+    background-color: #0000ff30;
+  }
+`;
 
 const BookListMobile = ({ books }) => {
   return (
     <div>
       <List className="book-list">
         {books.map((book) => (
-          <Link key={book.isbn13} to={`/book/product/${book.isbn13}`}>
-            <BookItemMobile book={book} />
-          </Link>
+          <BookItemContainer>
+            <Link key={book.isbn13} to={`/book/product/${book.isbn13}`}>
+              <BookItemMobile book={book} />
+            </Link>
+          </BookItemContainer>
         ))}
       </List>
     </div>
